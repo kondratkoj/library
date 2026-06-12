cb8a1c029b53d61b19d238437435388960f618b3
kondratkoj <220639277+kondratkoj@users.noreply.github.com>
2026-06-12 14:41:35 -0500

    Got the logic working for adding a book object to library array

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const Library = []
+
+function Book(title, author, pages) {
+  if (!new.target){throw Error("you have to use 'new' operator to call the constructor")}
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.UID = crypto.randomUUID()
+
+}
+
+function addBookToLibrary(title, author, pages) {
+  const book = new Book(title, author, pages)
+  Library.push(book)
+}
+
+addBookToLibrary("thisisatest", "madebyme", 420);
+
+addBookToLibrary("thisisanothertest", "madebyalsome", 421);
+
+console.log(Library)
